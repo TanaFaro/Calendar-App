4f16ac0f732a0edd47957f3d0331652a58406877
Ivana Farotto <ivanafarotto76@gmail.com>
2021-12-04 18:29:27 -0300

    inicial commit

---
+++ calendar-backend/README.md
@@ -0,0 +1,5 @@
+# Backend MERN - Calendar
+
+Backend de Calendar App
+
+ivanafarotto76@gmail.com
---
+++ calendar-frontend/src/router/AppRouter.js
@@ -0,0 +1,54 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Redirect
+} from "react-router-dom";
+
+import { LoginScreen } from '../components/auth/LoginScreen';
+import { CalendarScreen } from '../components/calendar/CalendarScreen';
+import { startChecking } from '../actions/auth';
+import { PublicRoute } from './PublicRouter';
+import { PrivateRoute } from './PrivateRouter';
+
+export const AppRouter = () => {
+
+    const dispatch = useDispatch();
+    const { checking, uid } = useSelector( state => state.auth);
+
+    useEffect(() => {
+        
+        dispatch( startChecking() );
+
+    }, [dispatch])
+
+    if ( checking ) {
+        return (<h5>Espere...</h5>);
+    }
+
+    return (
+        <Router>
+            <div>
+                <Switch>
+
+                    <PublicRoute 
+                        exact 
+                        path="/login" 
+                        component={ LoginScreen }
+                        isAuthenticated={ !!uid }
+                    />
+
+                    <PrivateRoute 
+                        exact 
+                        path="/" 
+                        component={ CalendarScreen } 
+                        isAuthenticated={ !!uid }
+                    />
+
+                    <Redirect to="/" />   
+                </Switch>
+            </div>
+        </Router>
+    )
+}
